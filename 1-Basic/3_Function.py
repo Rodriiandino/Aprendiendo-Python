78ffdb9a83d7838c23539e835a996543ba59a75f
RodriiAndino <andinorodrigo03@gmail.com>
2023-02-11 13:47:56 -0300

    Fix error

--- 1-Basic/3_Function.py
+++ 1-Basic/3_Function.py
@@ -41,7 +41,7 @@ print(num_str)                  # '10'
 
 # str to int or float
 num_str = '10.6'
-print('num_int', int(num_str))      # 10
+# print('num_int', int(num_str))      # 10
 print('num_float', float(num_str))  # 10.6
 
 # str to list
