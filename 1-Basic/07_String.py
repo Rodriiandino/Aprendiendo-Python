22b2d802689cfa98b3bb8d1a9dfe5528d59de9e9
RodriiAndino <andinorodrigo03@gmail.com>
2023-03-04 15:00:14 -0300

    add: more string examples

--- 1-Basic/07_String.py
+++ 1-Basic/07_String.py
@@ -6,6 +6,15 @@ print('spam eggs') # single quotes
 print(""" print on
          several lines """)
 
+
+# Escape Sequences in Strings
+"""
+\n: new line
+\t: Tab means(8 spaces)
+\\: Back slash
+\': Single quote (')
+\": Double quote (")
+"""
 print('doesn\'t') # use \' to escape the single quote...
 print("doesn't") # ...or use double quotes instead
 print('"Isn\'t," they said.') #combination
@@ -48,5 +57,66 @@ print(word[-2:])   # characters from the second-last (included) to the end //on
 print(word[:2] + word[2:]) # Python
 print(word[:4] + word[4:]) # Python
 
+# Reversing a String
+greeting = 'Hello, World!'
+print(greeting[::-1]) # !dlroW ,olleH
+
+
+
+# String formatting
+# The "%" operator is used to format a set of variables enclosed in a "tuple" (a fixed size list)
+"""
+%s - String 
+%d - Integers
+%f - Floating point numbers
+"%.number of digitsf" - Floating point numbers with fixed precision
+"""
+first_name = 'Rodrigo'
+last_name = 'Andino'
+language = 'Python'
+formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+print(formated_string) # I am Rodrigo Andino. I teach Python
+
+# Strings  and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area)
+print(formated_string) # The area of circle with a radius 10 is 314.00.
+
+
+# New Style String Formatting (str.format)
+
+first_name = 'Rodrigo'
+last_name = 'Andino'
+language = 'Python'
+formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
+print(formated_string)
+a = 4
+b = 3
+
+print('{} + {} = {}'.format(a, b, a + b)) # 7
+print('{} - {} = {}'.format(a, b, a - b)) # 1
+print('{} * {} = {}'.format(a, b, a * b)) # 12
+print('{} / {} = {:.2f}'.format(a, b, a / b)) # 1.33
+print('{} % {} = {}'.format(a, b, a % b)) # 1
+print('{} // {} = {}'.format(a, b, a // b)) # 1
+print('{} ** {} = {}'.format(a, b, a ** b)) # 64
+
+# Strings  and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of a circle with a radius {} is {:.2f}.'.format(radius, area)
+print(formated_string) #The area of a circle with a radius 10 is 314.00.
+
+
+# String Interpolation
+# Another new string formatting is string interpolation, f-strings. 
+# Strings start with f and we can inject the data in their corresponding positions.
+a = 4
+b = 3
+print(f'{a} + {b} = {a +b}') # 7 
+print(f'{a} - {b} = {a - b}') # 1
 
 
